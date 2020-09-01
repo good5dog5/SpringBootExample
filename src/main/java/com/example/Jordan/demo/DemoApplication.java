694bc01a0b3675335ba8b81fee1fabbb1fa90c3c
jordan <jordan.huang@gapp.nthu.edu.tw>
2020-09-01 14:39:03 +0800

    Add upload example.

--- src/main/java/com/example/Jordan/demo/DemoApplication.java
+++ src/main/java/com/example/Jordan/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package com.example.Jordan.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.Jordan.demo"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
